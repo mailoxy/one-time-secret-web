539ebce08046616fd9b5075587bd443db90417df
Mailoxy <roman@mailoxy.com>
2023-05-17 14:48:03 +0400

    Handle put errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
-<svelte:head>
-    <title>One Time Secret | Create </title> 
-</svelte:head>
-
 <script lang="ts">
-	import CryptoJS from "crypto-js";
-	import { PUBLIC_API_URL } from '$env/static/public'
+	import CryptoJS from 'crypto-js';
+	import { PUBLIC_API_URL } from '$env/static/public';
 
 	let secret = '';
 	let secretKey = '';
@@ -13,32 +9,36 @@
 	let encryptionKey = '';
 
 	function createSecret() {
-	  encryptionKey = CryptoJS.lib.WordArray.random(128/8).toString();
-	  encryptedSecret = CryptoJS.AES.encrypt(secret, encryptionKey).toString();
-	  // check if the secret is empty
-	  if (secret === '') {
-		alert('Please enter a secret');
-		return;
-	  }
-	  fetch(PUBLIC_API_URL, {
-		method: 'PUT',
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ encryptedSecret })
-	  })
-	  .then(response => response.json())
-	  .then(data => {
-		secretKey = data.secretKey;
-		const hostname = window.location.hostname;
-		const scheme = window.location.protocol;
-		const port = window.location.port;
-		if (port) {
-		  oneTimeSecretUrl = `${scheme}//${hostname}:${port}/secret/${secretKey}#${encryptionKey}`;
-		  return;
+		encryptionKey = CryptoJS.lib.WordArray.random(128 / 8).toString();
+		encryptedSecret = CryptoJS.AES.encrypt(secret, encryptionKey).toString();
+		// check if the secret is empty
+		if (secret === '') {
+			alert('Please enter a secret');
+			return;
 		}
-		oneTimeSecretUrl = `${scheme}//${hostname}/secret/${secretKey}#${encryptionKey}`;
-	  });
+		fetch(PUBLIC_API_URL, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ encryptedSecret })
+		})
+			.then((response) => response.json())
+			.catch((error) => {
+				console.error('Error:', error);
+				alert('Something went wrong. Please try again later.');
+			})
+			.then((data) => {
+				secretKey = data.secretKey;
+				const hostname = window.location.hostname;
+				const scheme = window.location.protocol;
+				const port = window.location.port;
+				if (port) {
+					oneTimeSecretUrl = `${scheme}//${hostname}:${port}/secret/${secretKey}#${encryptionKey}`;
+					return;
+				}
+				oneTimeSecretUrl = `${scheme}//${hostname}/secret/${secretKey}#${encryptionKey}`;
+			});
 	}
 
 	function createNewSecret() {
@@ -48,27 +48,47 @@
 	function copyToClipboard() {
 		navigator.clipboard.writeText(oneTimeSecretUrl);
 	}
-  
-  </script>
-  
-  <div class="flex flex-col justify-center items-center h-screen bg-gray-100 px-4">
+</script>
+
+<svelte:head>
+	<title>One Time Secret | Create</title>
+</svelte:head>
+
+<div class="flex flex-col justify-center items-center h-screen bg-gray-100 px-4">
 	<div class="space-y-4">
-	  <h1 class="text-3xl font-bold">One Time Secret</h1>
-	  {#if !oneTimeSecretUrl}
-	  <p class="text-gray-500">Create a secret and share it with your friends or collegues. Once they have seen it, it will be gone forever.</p>
-	  <div class="flex flex-col space-y-2"> 
-		<textarea class="border-2 border-gray-300 rounded-md p-2" bind:value={secret} placeholder="Type your secret here..." />
-		<button class="p-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded" on:click={createSecret}>Create Secret</button>
-	  </div>
-	  {/if}
-	  {#if oneTimeSecretUrl}
-		<p class="text-gray-500">Your secret has been created. Share the link below with your friends or colleagues.</p>
-		<div class="flex flex-col space-y-2">
-			<input class="border-2 border-gray-300 rounded-md p-2" bind:value={oneTimeSecretUrl} />
-			<button class="p-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded" on:click={copyToClipboard}>Copy to Clipboard</button>
-			<button class="p-2 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded" on:click={createNewSecret}>Create new secret</button>
-		</div>
-	  {/if}
+		<h1 class="text-3xl font-bold">One Time Secret</h1>
+		{#if !oneTimeSecretUrl}
+			<p class="text-gray-500">
+				Create a secret and share it with your friends or collegues. Once they have seen it, it will
+				be gone forever.
+			</p>
+			<div class="flex flex-col space-y-2">
+				<textarea
+					class="border-2 border-gray-300 rounded-md p-2"
+					bind:value={secret}
+					placeholder="Type your secret here..."
+				/>
+				<button
+					class="p-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+					on:click={createSecret}>Create Secret</button
+				>
+			</div>
+		{/if}
+		{#if oneTimeSecretUrl}
+			<p class="text-gray-500">
+				Your secret has been created. Share the link below with your friends or colleagues.
+			</p>
+			<div class="flex flex-col space-y-2">
+				<input class="border-2 border-gray-300 rounded-md p-2" bind:value={oneTimeSecretUrl} />
+				<button
+					class="p-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+					on:click={copyToClipboard}>Copy to Clipboard</button
+				>
+				<button
+					class="p-2 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+					on:click={createNewSecret}>Create new secret</button
+				>
+			</div>
+		{/if}
 	</div>
-  </div>
-  
+</div>
